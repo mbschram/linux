85fb178d1781f25fec9152a4a2c2e1927564bc81
Florian Fainelli <florian.fainelli@broadcom.com>
2016-03-14 16:57:09 -0700

    MIPS: BMIPS: Prepare for having bus-specific DMA operations
    Allow changing the dma_addr_to_phys and phys_to_dma functions, make the path
    looking for "brcm,ubus" set its own callbacks, while the default are providing
    an identity mapping unless changed.
    
    This sets the path for adding proper PCI dma operations which take care of the
    Broadcom STB specific holes.
    
    refs #SWLINUX-3868
    
    Signed-off-by: Florian Fainelli <florian.fainelli@broadcom.com>

--- arch/mips/bmips/dma.c
+++ arch/mips/bmips/dma.c
@@ -36,7 +36,29 @@ struct bmips_dma_range {
 	u32			size;
 };
 
+struct bmips_dma_ops {
+	dma_addr_t (*phys_to_dma)(struct device *dev, phys_addr_t pa);
+	unsigned long (*addr_to_phys)(struct device *dev, dma_addr_t dma);
+};
+
+static inline dma_addr_t bmips_noop_phys_to_dma(struct device *dev,
+						phys_addr_t pa)
+{
+	return pa;
+}
+
+static inline unsigned long bmips_noop_addr_to_phys(struct device *dev,
+						    dma_addr_t dma_addr)
+{
+	return dma_addr;
+}
+
 static struct bmips_dma_range *bmips_dma_ranges;
+
+static struct bmips_dma_ops bmips_dma_ops = {
+	.phys_to_dma = bmips_noop_phys_to_dma,
+	.addr_to_phys = bmips_noop_addr_to_phys,
+};
 
 #define FLUSH_RAC		0x100
 
@@ -67,7 +89,7 @@ static unsigned long bmips_ubus_dma_addr_to_phys(struct device *dev,
 
 static dma_addr_t bmips_phys_to_dma(struct device *dev, phys_addr_t pa)
 {
-	return bmips_ubus_phys_to_dma(dev, pa);
+	return bmips_dma_ops.phys_to_dma(dev, pa);
 }
 
 dma_addr_t plat_map_dma_mem(struct device *dev, void *addr, size_t size)
@@ -82,7 +104,7 @@ dma_addr_t plat_map_dma_mem_page(struct device *dev, struct page *page)
 
 unsigned long plat_dma_addr_to_phys(struct device *dev, dma_addr_t dma_addr)
 {
-	return bmips_ubus_dma_addr_to_phys(dev, dma_addr);
+	return bmips_dma_ops.addr_to_phys(dev, dma_addr);
 }
 
 static int __init bmips_init_dma_ranges(void)
@@ -115,6 +137,9 @@ static int __init bmips_init_dma_ranges(void)
 		r->parent_addr = be32_to_cpup(data++);
 		r->size = be32_to_cpup(data++);
 	}
+
+	bmips_dma_ops.phys_to_dma = bmips_ubus_phys_to_dma;
+	bmips_dma_ops.addr_to_phys = bmips_ubus_dma_addr_to_phys;
 
 out_good:
 	of_node_put(np);
