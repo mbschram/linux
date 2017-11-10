c03bff7e984db858af2b5796a29b6f274fcfa3e7
Florian Fainelli <f.fainelli@gmail.com>
2017-11-10 15:27:47 -0800

    net: dsa: bcm_sf2: Utilize b53_get_tag_protocol()
    b53_get_tag_protocol() is a much more capable version of
    bcm_sf2_sw_get_tag_protocol(), utilize the generic helper.
    
    Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>

--- drivers/net/dsa/b53/b53_priv.h
+++ drivers/net/dsa/b53/b53_priv.h
@@ -308,6 +308,7 @@ int b53_fdb_del(struct dsa_switch *ds, int port,
 		const unsigned char *addr, u16 vid);
 int b53_fdb_dump(struct dsa_switch *ds, int port,
 		 dsa_fdb_dump_cb_t *cb, void *data);
+enum dsa_tag_protocol b53_get_tag_protocol(struct dsa_switch *ds, int port);
 int b53_mirror_add(struct dsa_switch *ds, int port,
 		   struct dsa_mall_mirror_tc_entry *mirror, bool ingress);
 void b53_mirror_del(struct dsa_switch *ds, int port,
